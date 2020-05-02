e4c5bace02525dacce9438beff9169ba14a1abf1
Cristina Laryea <crispad06@gmail.com>
2020-05-01 20:25:01 -0400

    fixed private route

--- src/auth/PrivateRoute.js
+++ src/auth/PrivateRoute.js
@@ -1,26 +1,33 @@
 import React from "react";
-import { Route } from 'react-router-dom';
-import Login from '../components/Login';
-import axios from 'axios'
+import { Route } from "react-router-dom";
+import Login from "../components/Login";
+import axios from "axios";
 
-function isAuthd () {
-    const token = window.localStorage.getItem('token');
-    axios.post('https://sample-backend-c-r.herokuapp.com/api/auth/authenticate', {token})
-    .then(res => {
-        console.log(res.message)
-       return true  
+function isAuthd() {
+  const token = window.localStorage.getItem("token");
+
+  axios
+    .post("https://sample-backend-c-r.herokuapp.com/api/auth/authenticate", {
+      token,
     })
-    .catch(err => {
-        console.log(err)
-        return false
-    })       
+    .then((res) => {
+      console.log(res.message);
+      return true;
+    })
+    .catch((err) => {
+        window.location.href =`localhost:3000/login`
+      return false;
+    });
 }
-
 
 const PrivateRoute = (component) => {
-    console.log(component.component)
-    console.log(Login)
-   return <Route component = { isAuthd() ? component.component : Login } />
-}
+  console.log(component.component);
+  console.log(Login);
+  if (isAuthd() === true) {
+    return <Route component={component.component} />;
+  }
+  // history.push('/login')
+  return null;
+};
 
 export default PrivateRoute;
