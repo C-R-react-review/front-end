fa820039129675d29ecd5938b1407d55e0931393
Cristina Laryea <crispad06@gmail.com>
2020-05-02 23:08:34 -0400

    created a card list from get all users request, also implemented useState and useEffect hooks

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Card from './Card'
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import axios from "axios";
 
 function Feed() {
+  const [userList, setUserList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://sample-backend-c-r.herokuapp.com/api/users")
+      .then((res) => {
+        setUserList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },[]);
 
+// const texas = {
+//   population: 23123,
+//   capital: 'Austen',
+// }
+// console.log(texas.capital)
+  console.log(userList);
 
-
-
-  
   return (
     <div className="Feed">
-      <Card username='q' email='q@gmail.com' />
+      {userList.map((e) => {
+       return <Card username={e.username} email={e.email} />
+      })}
+    
     </div>
   );
 }
